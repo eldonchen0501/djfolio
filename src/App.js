fa1975bbdab08eeff6b371360e9be64a08b8d44c
Mickie Hsu <mickie.hsu@appfolio.com>
2018-07-27 11:25:15 -0700

    mh - replace url

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
   };
 
   getPlaylist = async () => {
-    const response = await fetch('/api/getPlaylist');
+    const response = await fetch('https://djfoliobackendbot.herokuapp.com/api/getPlaylist');
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
